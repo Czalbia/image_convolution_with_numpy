31a7cdee50d3ec6e28e306d7299599fe1295a886
czalbia <jan.czalba@gmail.com>
2024-01-08 00:15:44 +0100

    updates

--- image_as_source.py
+++ image_as_source.py
@@ -1,96 +1,79 @@
 import numpy as np
 from PIL import Image
+import time
 
+class ConvoluteValid():
 
-class convolute_valid():
-
-    def __init__(self, path, name):
-        self.image_done = 0
-        self.path = path
+    def __init__(self, name):
         self.name = name
-        # Importing the image as a grayscale array
-        self.frame = Image.open(path + "\\" + name).convert('L')
+        self.frame = Image.open(name).convert('L')
         self.feed_in_channel = np.asarray(self.frame)
-        # The kernel channel
-        # Here is an example of how it looks like
+        self.feed_in_channel2 = np.asarray(self.frame)
 
-        isChosen = True
-        while isChosen==True:
-            print('What kernel to use, press 1 for random, 2 for pre-coded one: ')
-            a = input()
-            if a == "1":
-                self.kernel_channel = np.random.randint(100,
-                                                        size=(100, 100))
-                isChosen=False
-            elif a == "2":
-                self.kernel_channel = np.array(
-                    [
-                        [1, 1, 2, 2, 1, 1],
-                        [1, 1, 2, 2, 1, 1],
-                        [1, 2, 6, 6, 2, 1],
-                        [1, 2, 6, 6, 2, 1],
-                        [1, 1, 2, 2, 1, 1],
-                        [1, 1, 2, 2, 1, 1]
+        self.kernel_channel = np.array([
+            [-1, 0, 1],
+            [-2, 0, 2],
+            [-1, 0, 1]
+        ])
+        self.kernel_channel2 = np.array([
+            [-1, -2, -1],
+            [0, 0, 0],
+            [1, 2, 1]
+        ])
 
-                    ]
-                )
-                isChosen = False
-
-    # My own dot product function
     def quick_dot(self, array1, array2):
-        array1 = array1 * array2
-        sum1 = sum(array1)
-        sum2 = 0
-        for i in sum1:
-            sum2 += i
-        return sum2
+        return np.sum(array1 * array2)
 
-    # The convolution happens here
     def convolution(self):
-        # Calculating how many steps it will take to convolute the whole image
         stepsX = self.feed_in_channel.shape[0] - self.kernel_channel.shape[0] + 1
         stepsY = self.feed_in_channel.shape[1] - self.kernel_channel.shape[1] + 1
 
         new = np.zeros([stepsX, stepsY], dtype=int)
+        new2 = np.zeros([stepsX, stepsY], dtype=int)
 
-        # Moving through the input channel
-        lastCount =0
         for i in range(stepsX):
+            start = time.time()
             for j in range(stepsY):
-                # The actual conv takes place here
-                # Croping the feed array so the dot product will work
                 new_input = self.feed_in_channel[i:self.kernel_channel.shape[0] + i, j:self.kernel_channel.shape[1] + j]
-                if (i!=lastCount):
-                    print("Running...", i, "//", stepsX)
-                    lastCount = i
+                new_input2 = self.feed_in_channel2[i:self.kernel_channel2.shape[0] + i,
+                                                  j:self.kernel_channel2.shape[1] + j]
                 new[i][j] = self.quick_dot(new_input, self.kernel_channel)
-        return new
+                new2[i][j] = self.quick_dot(new_input2, self.kernel_channel2)
+
+            if i % 10 == 0:
+                end = time.time()
+                time_took = end - start
+                print("Running...", i, "/", stepsX, "| it will take approximately:",
+                      round(((stepsX - i) * time_took) / 60), "minutes to compute")
+
+        return new, new2
 
     def run(self):
-        # The convoluted image
-        output_channel = self.convolution()
-
-        # Converting the pixels' values of convoluted image
-        # to a 0-255 gray-scale image
+        output_channel, output_channel2 = self.convolution()
 
         mini = np.amin(output_channel)
-        output_channel_prepared = output_channel
-        maxi = np.amax(output_channel_prepared)
+        maxi = np.amax(output_channel)
         output_channel_prepared = 255 * ((output_channel - mini) / (maxi - mini))
         output_channel_prepared = np.round(output_channel_prepared)
 
-        # The finished image is converted to an image
-        self.image_done = Image.fromarray(output_channel_prepared)
+        mini2 = np.amin(output_channel2)
+        maxi2 = np.amax(output_channel2)
+        output_channel_prepared2 = 255 * ((output_channel2 - mini2) / (maxi2 - mini2))
+        output_channel_prepared2 = np.round(output_channel_prepared2)
 
-    def save(self):
-        new_name = self.name+"_result.jpg"
-        self.image_done.save(self.path, new_name)
+        self.image_done = Image.fromarray(output_channel_prepared.astype('uint8'))
+        self.image_done2 = Image.fromarray(output_channel_prepared2.astype('uint8'))
+
+    def save_it(self):
+        new_name = "result_" + self.name
+        self.image_done.save(new_name)
 
     def show(self):
+        # Combining the images
+        self.image_done.paste(self.image_done2, (0, 0), self.image_done2)
         self.image_done.show()
 
 
-image = convolute_valid(r"C:\Users\jancz\my_stuff\pics", "IMG_5800.jpg")
+image = ConvoluteValid("car_tot.png")
 image.run()
-image.save()
 image.show()
